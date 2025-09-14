fbcbe058a120e15091f3a7c1f174c5f27ec1c137
kunalsonawane224 <sonawanekunal289@gmail.com>
2025-09-14 14:52:35 +0530

    database changes

--- config/database.php
+++ config/database.php
@@ -1,15 +1,23 @@
 <?php
+
 class Database {
     private $host = 'localhost';
+    private $port = '3307';
     private $db_name = 'automation_industry';
     private $username = 'root';
-    private $password = 'kunal';
+    private $password = '';
     private $conn;
+
     public function getConnection() {
         $this->conn = null;
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, 
-                                $this->username, $this->password);
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . 
+                ";port=" . $this->port . 
+                ";dbname=" . $this->db_name, 
+                $this->username, 
+                $this->password
+            );
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
